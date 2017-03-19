201f92237d19e9df2470fcc872454db8491e0faf
Peter Barcsak <peter.barcsak@gmail.com>
2017-03-19 20:47:31 +0000

    Exercise 7

---
+++ Projects/lrthw/ex07/drill_7.rb
@@ -0,0 +1,26 @@
+# prints a string
+puts "Mary had a little lamb."
+# prints a string, it treats the word snow in the #{} just as any other word
+puts "Its fleece was white as #{'snow'}."
+# prints another string
+puts "And everywhere that Mary went."
+# prints the . 10 times
+puts "." * 10 # what'd that do?
+# sets the end1 to 12 variables to a string(in case to a letter)
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+# prints the concatenated variables, the difference between print and puts is that print doesn't add a new line
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
